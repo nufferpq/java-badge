cdf77afeb1caac5331cb74ac8e544b42456af595
Perry Nuffer <perry.nuffer@familysearch.org>
2017-12-11 14:39:43 -0700

    update virtual method invocation interface

--- virtual-method-invocation/src/org/familysearch/java/badge/Main.java
+++ virtual-method-invocation/src/org/familysearch/java/badge/Main.java
@@ -1,46 +1,36 @@
 package org.familysearch.java.badge;
 
-
-import java.util.Collection;
 import java.util.Date;
-import java.util.HashSet;
-import java.util.Set;
 
-import jdk.nashorn.internal.ir.CatchNode;
-
-interface Car {
+interface Vehicle {
   int topSpeed(int enginePistonCount);
 }
 
-class VWVan implements Car {
+class VWVan implements Vehicle {
   public Date lastOilChange() {
     return new Date("24 November 2016");
   }
 
   public int topSpeed(int pistonCount) {
-    return 22 * pistonCount;
+    return 16 * pistonCount;
   }
 }
 
-class Delorean implements Car {
+class Delorean implements Vehicle {
 
   public int topSpeed(int pistonCount) {
-    return 33 * pistonCount;
-  }
-
-  public int topSpeed(int pistonCount, int transmissionGearCount) {
-    return 33 * pistonCount * transmissionGearCount ;
+    return 29 * pistonCount;
   }
 }
 
 public class Main {
 
   public static void main(String[] args) {
-    Car car1 = new VWVan();
-    Car car2 = new Delorean();
+    Vehicle vehicle1 = new VWVan();
+    Vehicle vehicle2 = new Delorean();
 
-    System.out.println("Vehicle1 topSpeed=" + car1.topSpeed(1));
+    System.out.println("Vehicle1 topSpeed=" + vehicle1.topSpeed(4));
 //    System.out.println("Van lastOilChange=" + car1.lastOilChange());
-    System.out.println("Vehicle2 topSpeed=" + car2.topSpeed(12));
+    System.out.println("Vehicle2 topSpeed=" + vehicle2.topSpeed(12));
   }
 }

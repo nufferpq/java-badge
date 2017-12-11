7a4c8b80e2101947f838f6286f60ca6d18b17c6e
Perry Nuffer <perry.nuffer@familysearch.org>
2017-12-11 00:16:50 -0700

    Update SortFile with 2 sort methods.

--- SortFile/src/org/familysearch/java/badge/Main.java
+++ SortFile/src/org/familysearch/java/badge/Main.java
@@ -3,18 +3,16 @@ package org.familysearch.java.badge;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 public class Main {
 
-  private static String fileLocation = "/home/nufferpq/sandbox/Main/src/org/familysearch/java/badge/";
+  private static String fileLocation = "/Users/nufferpq/Sandbox/java-badge/SortFile/src/org/familysearch/java/badge/";
 
   public static void main(String[] args) {
     try {
       sortFile2();
     }
-    adsfadf
     catch (Exception e) {
       System.out.println("Exception caught!");
       System.out.println("Exception Message: " + e.getMessage());
@@ -43,13 +41,17 @@ public class Main {
 
   public static void sortFile2() throws Exception {
     BufferedReader reader = new BufferedReader(new FileReader(fileLocation + "lorem.txt"));
+    ArrayList<String> arrayList=new ArrayList();
     String line="";
     while((line=reader.readLine())!=null){
-      map.put(getField(line),line);
+      arrayList.add(line);
     }
     reader.close();
+
+    Collections.sort(arrayList, String.CASE_INSENSITIVE_ORDER);
+
     FileWriter writer = new FileWriter(fileLocation + "lorem.out.txt");
-    for(String val : map.values()){
+    for(String val : arrayList){
       writer.write(val);
       writer.write('\n');
     }

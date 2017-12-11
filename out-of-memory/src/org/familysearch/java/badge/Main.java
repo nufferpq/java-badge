b3de6a5104f4f864f8d9eed5a19c05df72ef305b
Perry Nuffer <perry.nuffer@familysearch.org>
2017-12-11 13:51:09 -0700

    update out-of-memory

--- out-of-memory/src/org/familysearch/java/badge/Main.java
+++ out-of-memory/src/org/familysearch/java/badge/Main.java
@@ -1,6 +1,7 @@
 package org.familysearch.java.badge;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Main {
@@ -14,6 +15,15 @@ public class Main {
     System.out.println("-- out-of-memory program --");
     System.out.println("Begin: Available memory: " + getAvailableMemory());
     System.out.println("Begin: Free memory: " + getFreeMemory());
+
+    showMemory();
+    outOfMemoryErrorTest1();
+    System.gc();
+    outOfMemoryErrorTest2();
+    System.gc();
+    outOfMemoryErrorTest3();
+    System.gc();
+
     try {
       while (true) {
         myStringList.add(myString);
@@ -44,6 +54,83 @@ public class Main {
   private static long getFreeMemory() {
     Runtime runtime = Runtime.getRuntime();
     return runtime.freeMemory(); // out of the current heap, how much is free
+  }
+
+  private static void outOfMemoryErrorTest1() {
+    String myCharacters = "0123456789";
+    LinkedList<String> stringArrayList = new LinkedList<String>();
+    long itemCount = 0;
+    try {
+      while (true) {
+        stringArrayList.add(myCharacters);
+        itemCount++;
+      }
+    }
+    catch (OutOfMemoryError e2) {
+      System.out.println("Number of Items in ArrayList: " + itemCount);
+      System.out.println("OutOfMemoryError occurred: " + e2.getMessage());
+      e2.printStackTrace();
+      showMemory();
+    }
+  }
+
+  private static void outOfMemoryErrorTest2() {
+    String myCharacters = "0123456789";
+    ArrayList<String> stringArrayList = new ArrayList<String>();
+    long itemCount = 0;
+    try {
+      while (true) {
+        stringArrayList.add(new String(myCharacters));
+        itemCount++;
+      }
+    }
+    catch (OutOfMemoryError e2) {
+      System.out.println("Number of Items in ArrayList: " + itemCount);
+      System.out.println("OutOfMemoryError occurred: " + e2.getMessage());
+    }
+  }
+
+  private static void outOfMemoryErrorTest3() {
+    ArrayList<Character> charArrayList = new ArrayList<Character>();
+    long itemCount = 0;
+    try {
+      while (true) {
+        charArrayList.add(new Character('1'));
+        itemCount++;
+      }
+    }
+    catch (OutOfMemoryError e2) {
+      System.out.println("Number of Items in charArrayList: " + itemCount);
+      System.out.println("OutOfMemoryError occurred: " + e2.getMessage());
+    }
+
+  }
+
+  private static void showMemory() {
+    /* Total number of processors or cores available to the JVM */
+    System.out.println("Available processors (cores): " +
+        Runtime.getRuntime().availableProcessors());
+
+  /* Total amount of free memory available to the JVM */
+    System.out.println("Free memory (bytes): " +
+        Runtime.getRuntime().freeMemory());
+
+  /* This will return Long.MAX_VALUE if there is no preset limit */
+    long maxMemory = Runtime.getRuntime().maxMemory();
+  /* Maximum amount of memory the JVM will attempt to use */
+    System.out.println("Maximum memory (bytes): " +
+        (maxMemory == Long.MAX_VALUE ? "no limit" : maxMemory));
+
+  /* Total memory currently in use by the JVM */
+    System.out.println("Total memory (bytes): " +
+        Runtime.getRuntime().totalMemory());
+
+    long allocatedMemory =
+        (Runtime.getRuntime().totalMemory()-Runtime.getRuntime().freeMemory());
+
+    long presumableFreeMemory = Runtime.getRuntime().maxMemory() - allocatedMemory;
+
+    System.out.println("=====================FreeMemory: " + presumableFreeMemory + " ============================");
   }
 
 }
